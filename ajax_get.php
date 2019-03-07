41fe7bc343d12263fb8f11a75ae08377ac1db62c
umecoumeco <47265526+umecoumeco@users.noreply.github.com>
2019-03-08 01:20:42 +0900

    Add files via upload

--- ajax_get.php
+++ ajax_get.php
@@ -1,12 +1,28 @@
 <?php
 include('functions.php');
 
+//POSTデータの受け渡し
+//入力チェック(受信確認処理追加)
+// if (
+//     !isset($_POST['service_name']) || $_POST['service_name']==''
+// ) {
+//     exit('ParamError');
+// }
+
+// //POSTデータ取得
+// $service_name = $_POST['service_name'];
+
 //DB接続
 $pdo = db_conn();
 
 //データ表示SQL作成
 $sql = 'SELECT * FROM reviews_table';
+// $sql = 'SELECT * FROM reviews_table WHERE service_name = (:service_name)';
+
+// $service_name = $_POST['service_name'];
+
 $stmt = $pdo->prepare($sql);
+// $stmt->bindValue(':like_service_name', $service_name, PDO::PARAM_STR); 
 $status = $stmt->execute();
 
 //データ表示
